9d9bbbaa3ab84c6677a659d90e9be5917cbca74f
boyhagemann <boyhagemann@gmail.com>
2014-06-27 11:26:06 +0200

    update dashboard

--- app/routes.php
+++ app/routes.php
@@ -25,14 +25,16 @@ Route::get('/', function()
         $ready = null;
     }
 
-	return View::make('import', compact('stats', 'ready'));
+    $failed = FailedJob::paginate(10);
+
+	return View::make('import', compact('stats', 'ready', 'failed'));
 });
 
 Route::get('info', function() {
 
     Queue::push('ImportTradeTrackerCampaignInfo');
-    return Redirect::to('/')
-        ->withSuccess('The campaign info is now queued for update');
+
+    return Redirect::to('/')->withSuccess('The campaign info is now queued for update');
 });
 
 Route::get('/import/{id?}', function($id = null)
@@ -40,16 +42,14 @@ Route::get('/import/{id?}', function($id = null)
     // Load all feeds
     App::make('TradeTrackerImporter')->run($id);
 
-    return Redirect::to('/')
-        ->withSuccess('The product feed is now queued for import');
+    return Redirect::to('/')->withSuccess('The product feed is now queued for import');
 });
 
 Route::get('export', function()
 {
     Artisan::call('export:tasks');
 
-	return Redirect::to('/')
-        ->withSuccess('The tasks are now queued for export');
+	return Redirect::to('/')->withSuccess('The tasks are now queued for export');
 });
 
 Route::get('clicks', function()
@@ -57,7 +57,7 @@ Route::get('clicks', function()
     Queue::push('ExportTradeTrackerClicks')
         ->withSuccess('The clicks are now queued for export');
 
-    return Redirect::to('/');
+    return Redirect::to('/')->withSuccess('All clicks are exported');
 });
 
 
@@ -74,6 +74,13 @@ Route::get('delete', function()
     }
     catch(\Pheanstalk_Exception_ServerException $e){}
 
-    return Redirect::to('/')
-        ->withSuccess('All queued jobs are deleted succesfully');
+    return Redirect::to('/')->withSuccess('All queued jobs are deleted succesfully');
+});
+
+
+Route::get('quick/refresh', function() {
+
+    Artisan::call('migrate:refresh');
+
+    return Redirect::to('/') ->withSuccess('The database is now empty');
 });
--- app/views/import.blade.php
+++ app/views/import.blade.php
@@ -12,79 +12,127 @@
 
     <div class="container">
 
+
         @if(Session::has('success'))
-        <div class="alert alert-success">{{{ Session::get('success') }}}</div>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="alert alert-success">{{{ Session::get('success') }}}</div>
+            </div>
+        </div>
         @endif
 
-        <div class="col-lg-6">
+        <div class="row">
 
-            <h2>Import</h2>
-            <p>All the imports will create seperate queued jobs in the background.</p>
+            <div class="col-lg-5">
 
-            <h4>Info</h4>
-            <p>Gather commission information and other useful data from all campaigns.</p>
-            <p>{{ HTML::link('info', 'Update campaign info', ['class' => 'btn btn-primary btn-sm']) }}</p>
+                <h2>Import</h2>
+                <p>All the imports will create seperate queued jobs in the background.</p>
 
-            <h4>Product feeds</h4>
-            <p>Import the product feeds for these campaigns.</p>
-            <p>{{ HTML::link('import', 'Import all campaigns', ['class' => 'btn btn-warning btn-sm']) }}</p>
-            <p>{{ HTML::link('import/2626', 'Afvalemmershop.nl', ['class' => 'btn btn-default btn-sm']) }}</p>
-            <p>{{ HTML::link('import/867', 'Bestelkado.nl', ['class' => 'btn btn-default btn-sm']) }}</p>
-            <p>{{ HTML::link('import/1078', 'Algebeld.nl', ['class' => 'btn btn-default btn-sm']) }}</p>
+                <h4>Info</h4>
+                <p>Gather commission information and other useful data from all campaigns.</p>
+                <p>{{ HTML::link('info', 'Update campaign info', ['class' => 'btn btn-primary btn-sm']) }}</p>
 
-        </div>
+                <h4>Product feeds</h4>
+                <p>Import the product feeds for these campaigns.</p>
+                <p>{{ HTML::link('import', 'Import all campaigns', ['class' => 'btn btn-warning btn-sm']) }}</p>
+                <p>{{ HTML::link('import/2626', 'Afvalemmershop.nl', ['class' => 'btn btn-default btn-sm']) }}</p>
+                <p>{{ HTML::link('import/867', 'Bestelkado.nl', ['class' => 'btn btn-default btn-sm']) }}</p>
+                <p>{{ HTML::link('import/1078', 'Algebeld.nl', ['class' => 'btn btn-default btn-sm']) }}</p>
 
-        <div class="col-lg-6">
+            </div>
 
-            <h2>Export</h2>
-            <p>This will export the data to the taskreward application</p>
+            <div class="col-lg-5">
 
-            <h4>Tasks</h4>
-            <p>
-                Export all tasks that are ready.
-            </p>
-            <p>
-                {{ HTML::link('export', 'Export', ['class' => 'btn btn-primary btn-sm']) }}
-            </p>
+                <h2>Export</h2>
+                <p>This will export the data to the taskreward application</p>
 
-            <h4>Clicks</h4>
-            <p>
-                Get the clicks registered from the affiliate parties and export them to the taskreward application.
-            </p>
-            <p>
-                {{ HTML::link('clicks', 'Export clicks', ['class' => 'btn btn-primary btn-sm']) }}
-            </p>
+                <h4>Tasks</h4>
+                <p>
+                    Export all tasks that are ready.
+                </p>
+                <p>
+                    {{ HTML::link('export', 'Export', ['class' => 'btn btn-primary btn-sm']) }}
+                </p>
 
-        </div>
+                <h4>Clicks</h4>
+                <p>
+                    Get the clicks registered from the affiliate parties and export them to the taskreward application.
+                </p>
+                <p>
+                    {{ HTML::link('clicks', 'Export clicks', ['class' => 'btn btn-primary btn-sm']) }}
+                </p>
 
-        <div class="col-lg-12">
+            </div>
 
-            <hr>
+            <div class="col-lg-2">
 
+                <h2>Quick</h2>
 
-            @if($ready)
+                <p>{{ HTML::link('quick/refresh', 'Refresh database') }}</p>
 
-            <h2>Queue <span class="badge badge-danger">{{ $stats['current-jobs-ready'] }}</span></h2>
-
-            <h4>Next job</h4>
-            <section>
-                <pre style=" display: table-cell; white-space: pre-line; padding: 10px">
-                    <strong>{{ $ready->getId() }}</strong> {{ trim($ready->getData()) }}
-                </pre>
-            </section>
-
-
-            @else
-
-            <h2>Queue <span class="badge">0</span></h2>
-
-            <p>There are no jobs on the queue</p>
-
-            @endif
+            </div>
 
         </div>
 
+        <div class="row">
 
+            <div class="col-lg-12">
+
+                <hr>
+
+
+                @if($ready)
+
+                <h2>Queue <span class="badge badge-danger">{{ $stats['current-jobs-ready'] }}</span></h2>
+
+                <h4>Next job</h4>
+                <section>
+                    <pre style=" display: table-cell; white-space: pre-line; padding: 10px">
+                        <strong>{{ $ready->getId() }}</strong> {{ trim($ready->getData()) }}
+                    </pre>
+                </section>
+
+
+                @else
+
+                <h2>Queue <span class="badge">0</span></h2>
+
+                <p>There are no jobs on the queue</p>
+
+                @endif
+
+            </div>
+
+        </div>
+
+        @if($failed->count())
+        <div class="row">
+
+            <div class="col-lg-12">
+
+                <hr>
+
+                <h2>Failed jobs</h2>
+
+                <table class="table">
+                @foreach($failed as $job)
+                    <tr>
+                        <td>{{ $job->failed_at }}</td>
+                        <td>{{ $job->connection }}</td>
+                        <td>{{ $job->queue }}</td>
+                        <td><pre>{{ $job->payload }}</pre></td>
+                    </tr>
+                @endforeach
+                </table>
+
+                {{ $failed->links() }}
+
+                {{ HTML::link('quick/refresh', 'Refresh database', ['class' => 'btn btn-danger btn-sm']) }}
+
+            </div>
+
+        </div>
+        @endif
     </div>
 
 
